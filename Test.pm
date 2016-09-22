96f473e3ab0a162c58579996e881e1bef4df963a
dj-tech <www.giuseppe@dellavedova.org>
2016-09-22 14:22:08 +0200

    Update Test.pm

--- Test.pm
+++ Test.pm
@@ -1,1 +1,14 @@
 print (<>)
+print “Scegli un numeron”;
+$num=<StdIN>;
+if ($num == ‘0’) {
+print “Hai premuto 0n”;
+} elsif ($num == ‘1’) {
+print “Hai premuto unon”;
+} elsif ($numero == ‘2’) {
+print “Hai premuto duen”;
+} elsif ($num == ‘3’) {
+print “Hai premuto tren”;
+} else {
+print “Non hai premuto nè zero nè uno nè due nè tre!n”;
+}
